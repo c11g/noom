b2186a10d24abe91763d017fa01e8ffa8e6799ab
choyoungkwang <choyoungkwang@nts-corp.com>
2021-10-14 18:25:37 +0900

    #1 Chat With WebSockets

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,42 @@
-console.log("hello!");
+const socket = new WebSocket(`ws://${location.host}`);
+const chatList = document.querySelector("#chatList");
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const nicknameSpan = document.querySelector("#nickname");
+const nicknameInput = document.querySelector("#nicknameInput");
+const nicknameButton = document.querySelector("#nicknameButton");
+let nickname = "Anonymous";
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server");
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server");
+});
+
+socket.addEventListener("message", (event) => {
+  const p = document.createElement("p");
+  p.innerText = event.data;
+  chatList.appendChild(p);
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = input.value;
+
+  socket.send(
+    JSON.stringify({
+      nickname,
+      message,
+    })
+  );
+  input.value = "";
+});
+
+nicknameButton.addEventListener("click", () => {
+  if (!nicknameInput.value) return;
+  nickname = nicknameInput.value;
+  nicknameSpan.innerHTML = nickname;
+  nicknameInput.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -13,10 +13,27 @@ app.use("/public", express.static(`${__dirname}/public`));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
-// app.listen(PORT, handleListen);
-
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-server.listen(PORT, handleListen);
+const sockets = [];
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser");
+
+  sockets.push(socket);
+
+  socket.on("message", (messageObj) => {
+    const { nickname, message } = JSON.parse(messageObj);
+    sockets.forEach((aSocket) => {
+      aSocket.send(
+        `${nickname}${socket === aSocket ? "(ME)" : ""}: ${message}`
+      );
+    });
+  });
+
+  socket.on("close", () => console.log("Disconnected from Browswer"));
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
