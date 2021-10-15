5db9f63663f40a09f1f5c6a570bc0c41bec01b1a
choyoungkwang <choyoungkwang@nts-corp.com>
2021-10-15 13:47:49 +0900

    remove websocket code

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import http from "http";
 import express from "express";
-import { WebSocketServer } from "ws";
 
 const app = express();
 const PORT = 3000;
@@ -14,26 +13,26 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
-
-const sockets = [];
-wss.on("connection", (socket) => {
-  console.log("Connected to Browser");
-
-  sockets.push(socket);
-
-  socket.on("message", (messageObj) => {
-    const { nickname, message } = JSON.parse(messageObj);
-    sockets.forEach((aSocket) => {
-      aSocket.send(
-        `${nickname}${socket === aSocket ? "(ME)" : ""}: ${message}`
-      );
-    });
-  });
-
-  socket.on("close", () => console.log("Disconnected from Browswer"));
-});
 
 server.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
+
+// 확인용 주석
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//   console.log("Connected to Browser");
+
+//   sockets.push(socket);
+
+//   socket.on("message", (messageObj) => {
+//     const { nickname, message } = JSON.parse(messageObj);
+//     sockets.forEach((aSocket) => {
+//       aSocket.send(
+//         `${nickname}${socket === aSocket ? "(ME)" : ""}: ${message}`
+//       );
+//     });
+//   });
+
+//   socket.on("close", () => console.log("Disconnected from Browswer"));
+// });
